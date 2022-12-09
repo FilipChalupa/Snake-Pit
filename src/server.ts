f2dbef38d7ebc4b0ae1606af43819627e0b3c462
Filip Chalupa <chalupa.filip@gmail.com>
2022-12-09 18:38:06 +0100

    Prepare endpoints

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,21 @@
-console.log('Server is running')
+import express from 'express'
+
+const app = express()
+
+app.get('/list-rooms', (request, response) => {
+	response.send('Ok')
+})
+app.post('/create-room', (request, response) => {
+	response.send('Ok')
+})
+app.get('/room/:id', (request, response) => {
+	response.send('Ok')
+})
+app.post('/room/:id', (request, response) => {
+	response.send('Ok')
+})
+
+app.use(express.static('public'))
+
+app.listen(3000)
+console.log('Server is running on port 3000')
