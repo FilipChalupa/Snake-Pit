4c52e6872afd152941a5298bbe1f17580a4948da
Filip Chalupa <chalupa.filip@gmail.com>
2022-12-10 19:15:49 +0100

    Demo bot: be more random

--- public/assets/demo-bot.js
+++ public/assets/demo-bot.js
@@ -74,20 +74,52 @@ const loop = async (action) => {
 	const player = players.find((otherPlayer) => otherPlayer.id === yourPlayerId)
 	let nextAction = 'forward'
 	if (player?.isAlive) {
+		const isObstacle = (() => {
+			const obstacles = {}
+			for (let i = -1; i <= width; i++) {
+				obstacles[i] = {}
+				obstacles[i][-1] = true
+				obstacles[i][height] = true
+			}
+			for (let i = 0; i < height; i++) {
+				obstacles[-1][i] = true
+				obstacles[width][i] = true
+			}
+			return (x, y) => obstacles[x]?.[y] ?? false
+		})()
 		const headPosition = player.fromHeadPosition[0]
 		const neckPosition = player.fromHeadPosition[1]
 		const direction = {
 			x: headPosition.x - neckPosition.x,
 			y: headPosition.y - neckPosition.y,
 		}
-		if (direction.x === 1 && headPosition.x === width - 1) {
-			nextAction = headPosition.y < height / 2 ? 'right' : 'left'
-		} else if (direction.x === -1 && headPosition.x === 0) {
-			nextAction = headPosition.y < height / 2 ? 'left' : 'right'
-		} else if (direction.y === 1 && headPosition.y === height - 1) {
-			nextAction = headPosition.x < height / 2 ? 'left' : 'right'
-		} else if (direction.y === -1 && headPosition.y === 0) {
-			nextAction = headPosition.x < height / 2 ? 'right' : 'left'
+		const isForwardFree = !isObstacle(
+			headPosition.x + direction.x,
+			headPosition.y + direction.y,
+		)
+		const isLeftFree = !isObstacle(
+			headPosition.x + direction.y,
+			headPosition.y - direction.x,
+		)
+		const isRightFree = !isObstacle(
+			headPosition.x - direction.y,
+			headPosition.y + direction.x,
+		)
+		const options = []
+		if (isForwardFree) {
+			// Increase forward probability
+			for (let i = 0; i < 8; i++) {
+				options.push('forward')
+			}
+		}
+		if (isLeftFree) {
+			options.push('left')
+		}
+		if (isRightFree) {
+			options.push('right')
+		}
+		if (options.length > 0) {
+			nextAction = options[Math.floor(Math.random() * options.length)]
 		}
 	}
 	// @TODO: remove delay
